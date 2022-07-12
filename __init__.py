212a1142bda88f6ba16b87ec82cbd9a1d96df634
iamjustkoi <iamjustkoi@gmail.com>
2022-07-12 15:27:48 -0400

    Added almost-leech marker option

--- __init__.py
+++ __init__.py
@@ -2,11 +2,12 @@
 MIT License: Copyright (c) 2022 JustKoi (iamjustkoi) <https://github.com/iamjustkoi>
 Full license text available in "LICENSE" file packaged with the program.
 """
-from .src import actions
+from .src import options, reviewer
 
 
 def init():
-    actions.bind_options_actions()
+    options.bind_actions()
+    reviewer.build_hooks()
     pass
 
 
