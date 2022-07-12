1d64e316952a139448541e4f08cd32e6f909cbf9
iamjustkoi <iamjustkoi@gmail.com>
2022-07-12 15:27:30 -0400

    Added almost-leech marker option

--- src/options.py
+++ src/options.py
@@ -2,12 +2,46 @@
 MIT License: Copyright (c) 2022 JustKoi (iamjustkoi) <https://github.com/iamjustkoi>
 Full license text available in "LICENSE" file packaged with the program.
 """
-from aqt.qt import QDialog
+from aqt import mw
+from aqt.qt import QAction, QDialog
 
-from . import actions
 from .config import LeechToolkitConfigManager
-from .consts import Config
-from res.ui.options_dialog import Ui_OptionsDialog
+from .consts import String, Config
+from ..res.ui.options_dialog import Ui_OptionsDialog
+
+
+def bind_actions():
+    _bind_config_options()
+    _bind_tools_options()
+
+
+def on_options_called(result=False):
+    options = OptionsDialog(LeechToolkitConfigManager(mw))
+    options.exec()
+
+
+def _bind_tools_options(changes=None, obj=None):
+    """
+Updates the toolbar actions menu with the options shortcut. Expects an Operation Change hook call,
+but can also be used as a general update push, too.
+    :param changes: unused OpChanges object
+    :param obj: unused options object
+    """
+    config = LeechToolkitConfigManager(mw).config
+    if config[Config.TOOLBAR_ENABLED]:
+        options_action = QAction(String.TOOLBAR_OPTIONS, mw)
+        options_action.triggered.connect(on_options_called)
+        # Handles edge cases where toolbar action already exists in the tools menu
+        if options_action.text() not in [action.text() for action in mw.form.menuTools.actions()]:
+            mw.form.menuTools.addAction(options_action)
+    else:
+        for action in mw.form.menuTools.actions():
+            if action.text() == String.TOOLBAR_OPTIONS:
+                mw.form.menuTools.removeAction(action)
+
+
+def _bind_config_options():
+    mw.addonManager.setConfigAction(__name__, on_options_called)
 
 
 class OptionsDialog(QDialog):
@@ -23,12 +57,14 @@ class OptionsDialog(QDialog):
 
     def _load(self):
         self.ui.toolsOptionsCheckBox.setChecked(self.config[Config.TOOLBAR_ENABLED])
+        self.ui.almostMarkCheckbox.setChecked(self.config[Config.SHOW_ALMOST_LEECH_MARKER])
 
     def _save(self):
         self.config[Config.TOOLBAR_ENABLED] = self.ui.toolsOptionsCheckBox.isChecked()
+        self.config[Config.SHOW_ALMOST_LEECH_MARKER] = self.ui.almostMarkCheckbox.isChecked()
         self.manager.write_config()
 
     def accept(self) -> None:
         self._save()
         super().accept()
-        actions.bind_options_actions()
+        bind_actions()
