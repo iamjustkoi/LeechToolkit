3e6a143a89fd2ed8c29d1184629eafa5306f98a1
iamjustkoi <iamjustkoi@gmail.com>
2022-07-12 10:43:16 -0400

    Initial commits

---
+++ src/config.py
@@ -0,0 +1,44 @@
+"""
+MIT License: Copyright (c) 2022 JustKoi (iamjustkoi) <https://github.com/iamjustkoi>
+Full license text available in "LICENSE" file packaged with the program.
+"""
+from typing import Union, Dict, Any, Annotated
+
+from aqt import AnkiQt
+from .consts import Config
+
+
+class ConfigManager:
+
+    def __init__(self, mw: AnkiQt):
+        """
+Config manager for accessing and writing addon config values.
+
+    :param mw: Anki window to retrieve addon config data from
+        """
+        super().__init__()
+        self.mw = mw
+        self._addon = self.mw.addonManager.addonFromModule(__name__)
+        self._meta = self.mw.addonManager.addonMeta(self._addon)
+
+        self.config = self._meta.get('config', Config.DEFAULT_CONFIG)
+        self.decks = self.mw.col.decks if self.mw.col is not None else None
+
+        for field in Config.DEFAULT_CONFIG:
+            if field not in self.config:
+                # load temp defaults
+                self.config[field] = Config.DEFAULT_CONFIG[field]
+
+        self._meta['config'] = self.config
+
+    def write_config(self):
+        """
+Writes the config manager's current values to the addon meta file.
+        """
+        self.mw.addonManager.writeAddonMeta(self._addon, self._meta)
+
+    def refresh_config(self):
+        """
+Placeholder for config refreshes/rewrites per-updates, etc.
+        """
+
---
+++ src/consts.py
@@ -0,0 +1,20 @@
+"""
+MIT License: Copyright (c) 2022 JustKoi (iamjustkoi) <https://github.com/iamjustkoi>
+Full license text available in "LICENSE" file packaged with the program.
+"""
+
+CURRENT_VERSION = '0.0.1a'
+
+ANKI_LEGACY_VER = 35
+
+
+class Config:
+    TOOLBAR_ENABLED = 'Use_Toolbar_Options'
+    DEFAULT_CONFIG = {
+        TOOLBAR_ENABLED: True
+    }
+
+
+class String:
+    TOOLBAR_OPTIONS = 'Leech Toolkit Options...'
+    pass
