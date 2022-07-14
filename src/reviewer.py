3d93d7913c386272543e27ba4dcecdaf80147ea4
iamjustkoi <jtritnum@gmail.com>
2022-07-14 09:20:25 -0400

    Implemented marker position functions

--- src/reviewer.py
+++ src/reviewer.py
@@ -8,19 +8,20 @@ from aqt import webview
 from aqt import mw, gui_hooks
 
 from .config import LeechToolkitConfigManager
-from .consts import Config
+from .consts import Config, MARKER_POS_STYLES
 
 from_lapse = 1
 marker_id = 'leech_marker'
 marker_text = '🩸'
 marker_color = 'rgb(248, 197, 86)'
+# marker_float = 'unset'
 almost_leech_html = '''
 <style>
     #{marker_id} {{
         color: transparent;  
         text-shadow: 0 0 0 {marker_color};
         font-size: .4em !important;
-        float: right;
+        float: {marker_float};
         display: none;
     }}
 </style>
@@ -38,12 +39,14 @@ def build_hooks():
 
 
 def on_will_start(content: webview.WebContent, context: reviewer.Reviewer):
-    global almost_leech_html
-    if LeechToolkitConfigManager(mw).config[Config.SHOW_ALMOST_LEECH_MARKER]:
+    user_conf = LeechToolkitConfigManager(mw).config
+    if user_conf[Config.SHOW_ALMOST_LEECH_MARKER]:
+        marker_float = MARKER_POS_STYLES[user_conf[Config.ALMOST_MARKER_POSITION]]
         content.body += almost_leech_html.format(
             marker_id=marker_id,
             marker_text=marker_text,
-            marker_color=marker_color
+            marker_color=marker_color,
+            marker_float=marker_float
         )
 
 
