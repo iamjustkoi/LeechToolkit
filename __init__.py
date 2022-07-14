e2e2b8cfa914c67a842d63c1a72a1f245c575dc4
iamjustkoi <iamjustkoi@gmail.com>
2022-07-14 12:45:14 -0400

    Added bottombar.py imports

--- __init__.py
+++ __init__.py
@@ -2,12 +2,13 @@
 MIT License: Copyright (c) 2022 JustKoi (iamjustkoi) <https://github.com/iamjustkoi>
 Full license text available in "LICENSE" file packaged with the program.
 """
-from .src import options, reviewer
+from .src import options, reviewer, bottombar
 
 
 def init():
     options.bind_actions()
     reviewer.build_hooks()
+    bottombar.build_bottom_bar()
     pass
 
 
