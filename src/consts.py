5a58c5a37546ccfb7b7afe52bc996caa6cda4384
iamjustkoi <jtritnum@gmail.com>
2022-07-14 13:11:09 -0400

    Version update

--- src/consts.py
+++ src/consts.py
@@ -4,7 +4,7 @@ Full license text available in "LICENSE" file packaged with the program.
 """
 from typing import NewType
 
-CURRENT_VERSION = '0.0.1a'
+CURRENT_VERSION = '0.0.2a'
 
 ANKI_LEGACY_VER = 35
 
