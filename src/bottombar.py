a2fc9bcb664279458735a70f2a3feac12752ea3e
iamjustkoi <jtritnum@gmail.com>
2022-07-15 11:00:27 -0400

    Added leech recognition to browse button

--- src/bottombar.py
+++ src/bottombar.py
@@ -13,21 +13,29 @@ from .consts import String, LEECHES_URL
 def build_bottom_bar():
 
     def draw_bottom_bar(self, buf: str, web_context, link_handler):
-        default_link_handler = link_handler
+        if isinstance(web_context, (OverviewBottomBar, DeckBrowserBottomBar)):
 
-        def link_handler(url):
-            if isinstance(web_context, (OverviewBottomBar, DeckBrowserBottomBar)) and url == LEECHES_URL:
-                leech_flag = 'tag:leech'
-                deck_flag = 'deck:current' if isinstance(web_context, OverviewBottomBar) else 'deck:*'
-                dialogs.open("Browser", mw, search=(leech_flag, deck_flag))
+            leech_search_flag = 'tag:leech'
+            deck_search_flag = 'deck:current' if mw.state == 'overview' else 'deck:*'
 
-            default_link_handler(url=url)
+            total_leeches = len(mw.col.find_cards(f'{leech_search_flag} {deck_search_flag}'))
 
-        def updated_buf(default_buf):
-            button = BarButton(String.VIEW_LEECHES, LEECHES_URL)
-            return '\n'.join([default_buf, button.html])
+            if total_leeches > 0:
+                default_link_handler = link_handler
 
-        return default_draw(self, buf=updated_buf(buf), link_handler=link_handler, web_context=web_context)
+                def leech_link_handler(url):
+                    if url == LEECHES_URL:
+                        dialogs.open("Browser", mw, search=(leech_search_flag, deck_search_flag))
+
+                    default_link_handler(url=url)
+
+                def updated_buf(default_buf):
+                    button = BarButton(String.VIEW_LEECHES, LEECHES_URL)
+                    return '\n'.join([default_buf, button.html])
+
+                return default_draw(self, buf=updated_buf(buf), link_handler=leech_link_handler, web_context=web_context)
+
+        return default_draw(self, buf=buf, link_handler=link_handler, web_context=web_context)
 
     default_draw = BottomBar.draw
     BottomBar.draw = draw_bottom_bar
