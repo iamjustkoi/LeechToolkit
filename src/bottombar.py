8adb3801ed6d462d2b50bac9b88528cc13d60cfe
iamjustkoi <jtritnum@gmail.com>
2022-07-15 10:07:49 -0400

    Added leech browsing functionality

--- src/bottombar.py
+++ src/bottombar.py
@@ -5,60 +5,26 @@ Full license text available in "LICENSE" file packaged with the program.
 from aqt.toolbar import BottomBar
 from aqt.deckbrowser import DeckBrowserBottomBar
 from aqt.overview import OverviewBottomBar
-from aqt import gui_hooks, webview
+from aqt import dialogs, mw
 
 from .consts import String
 
 
-# def build_hooks():
-#     gui_hooks.webview_will_set_content.append(
-#         lambda content, context:
-#         build_bottom_bar(content, context)
-#     )
-
-
 def build_bottom_bar():
 
-    # bottom = aqt.toolbar.BottomBar
-    # buf = ""
-    # drawLinks = deepcopy(self.drawLinks)
-    # for b in drawLinks:
-    #     if b[0]:
-    #         b[0] = tr.actions_shortcut_key(val=shortcut(b[0]))
-    #     buf += """
-    # <button title='%s' onclick='pycmd(\"%s\");'>%s</button>""" % tuple(
-    #         b
-    #     )
-    # bottom.draw(
-    #     buf=buf,
-    #     link_handler=_linkHandler,
-    #     web_context=DeckBrowserBottomBar(),
-    # )
-
     def draw_bottom_bar(self, buf: str, web_context, link_handler):
-
-        if not (isinstance(web_context, DeckBrowserBottomBar) or isinstance(web_context, OverviewBottomBar)):
-            print(f'\n  NOT INSTANCE')
-            return
-
         default_link_handler = link_handler
-
         edit_leech_url = 'editleeches'
 
-        # if isinstance(bottom_bar, DeckBrowserBottomBar):
-        #     print(f'\n  content: {type(bottom_bar)}')
-        #     edit_button = BarButton(String.EDIT_LEECHES, edit_leech_url)
-        # elif isinstance(bottom_bar, OverviewBottomBar):
-        #     edit_button = BarButton(String.EDIT_LEECHES, edit_leech_url)
-        #     # default_draw(url=url)
-
         def link_handler(url):
-            if url == edit_leech_url:
-                print(f'\n Editing Leeches...')
+            if isinstance(web_context, (OverviewBottomBar, DeckBrowserBottomBar)) and url == edit_leech_url:
+                leech_flag = 'tag:leech'
+                deck_flag = 'deck:current' if isinstance(web_context, OverviewBottomBar) else 'deck:*'
+                dialogs.open("Browser", mw, search=(leech_flag, deck_flag))
             default_link_handler(url=url)
 
         def updated_buf(default_buf):
-            button = BarButton(String.EDIT_LEECHES, 'editleeches')
+            button = BarButton(String.VIEW_LEECHES, edit_leech_url)
             return '\n'.join([default_buf, button.html])
 
         return default_draw(self, buf=updated_buf(buf), link_handler=link_handler, web_context=web_context)
