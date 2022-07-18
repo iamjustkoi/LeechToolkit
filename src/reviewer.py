aac0038c34aa94c673a39774eb54dde888c93a83
iamjustkoi <iamjustkoi@gmail.com>
2022-07-18 14:59:32 -0400

    Added lapse decrease functionality

--- src/reviewer.py
+++ src/reviewer.py
@@ -6,7 +6,7 @@ import anki.cards
 import aqt.reviewer
 from aqt import reviewer
 from aqt import webview
-from aqt import mw, gui_hooks
+from aqt import mw, gui_hooks, utils
 
 from .config import LeechToolkitConfigManager
 from .consts import Config, MARKER_POS_STYLES
@@ -32,17 +32,22 @@ marker_id = 'leech_marker'
 marker_text = '🩸'
 marker_color = 'rgb(248, 197, 86)'
 
+decrease_enabled = True
+tooltip_enabled = True
+
 
 def build_hooks():
     from aqt.gui_hooks import webview_will_set_content
     from aqt.gui_hooks import reviewer_did_show_question
     from aqt.gui_hooks import reviewer_did_show_answer
+    from aqt.gui_hooks import reviewer_did_answer_card
     webview_will_set_content.append(
         lambda content, context:
         on_will_start(content, context) if isinstance(context, reviewer.Reviewer) else None
     )
     reviewer_did_show_question.append(on_did_show_question)
     reviewer_did_show_answer.append(on_did_show_answer)
+    reviewer_did_answer_card.append(on_did_answer)
 
 
 def on_will_start(content: aqt.webview.WebContent, context: aqt.reviewer.Reviewer):
@@ -50,6 +55,7 @@ def on_will_start(content: aqt.webview.WebContent, context: aqt.reviewer.Reviewe
     conf = mw.col.decks.config_dict_for_deck_id(mw.col.decks.get_current_id())
     max_fails = conf['lapse']['leechFails']
     user_conf = LeechToolkitConfigManager(mw).config
+
     append_html(content)
 
 
@@ -62,6 +68,9 @@ def append_html(content: aqt.webview.WebContent):
 
 
 def on_did_show_answer(card: anki.cards.Card):
+    if decrease_enabled:
+        setattr(card, 'prevtype', card.type)
+
     if card.type == anki.cards.CARD_TYPE_REV and (card.lapses + from_lapse) >= max_fails:
         show_marker(True)
 
@@ -72,6 +81,17 @@ def on_did_show_question(card: anki.cards.Card):
     else:
         if card.type == anki.cards.CARD_TYPE_REV and (card.lapses + from_lapse) >= max_fails:
             show_marker(True)
+
+
+def on_did_answer(context: aqt.reviewer.Reviewer, card: anki.cards.Card, ease: int):
+    if hasattr(card, 'prevtype'):
+        if ease > 1 and card.prevtype != anki.cards.CARD_TYPE_RELEARNING and card.lapses > 0:
+            card.lapses -= 1
+            card.flush()
+            if tooltip_enabled:
+                utils.tooltip(f'Card\'s lapses set to: {card.lapses}', x_offset=400)
+
+        delattr(card, 'prevtype')
 
 
 def show_marker(show=False):
