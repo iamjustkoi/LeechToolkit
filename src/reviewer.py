61fb1b52c8637a68b08328dbc52dea1822a176f7
iamjustkoi <jtritnum@gmail.com>
2022-07-19 10:42:17 -0400

    Implemented decrease reverse options

--- src/reviewer.py
+++ src/reviewer.py
@@ -7,9 +7,10 @@ import aqt.reviewer
 from aqt import reviewer
 from aqt import webview
 from aqt import mw, gui_hooks, utils
+from anki import cards
 
 from .config import LeechToolkitConfigManager
-from .consts import Config, MARKER_POS_STYLES
+from .consts import Config, MARKER_POS_STYLES, REV_DECREASE
 
 conf: dict
 max_fails: int
@@ -31,6 +32,7 @@ from_lapse = 1
 marker_id = 'leech_marker'
 marker_text = '🩸'
 marker_color = 'rgb(248, 197, 86)'
+prev_type_attr = 'prevtype'
 
 decrease_enabled = True
 tooltip_enabled = True
@@ -67,31 +69,33 @@ def append_html(content: aqt.webview.WebContent):
         )
 
 
-def on_did_show_answer(card: anki.cards.Card):
-    if decrease_enabled:
-        setattr(card, 'prevtype', card.type)
+def on_did_show_answer(card: cards.Card):
+    # user_conf[Config.]
+    if user_conf[Config.REVERSE_ENABLED]:
+        setattr(card, prev_type_attr, card.type)
 
-    if card.type == anki.cards.CARD_TYPE_REV and (card.lapses + from_lapse) >= max_fails:
+    if card.type == cards.CARD_TYPE_REV and (card.lapses + from_lapse) >= max_fails:
         show_marker(True)
 
 
-def on_did_show_question(card: anki.cards.Card):
+def on_did_show_question(card: cards.Card):
     if user_conf[Config.ALMOST_ON_BACK]:
         show_marker(False)
     else:
-        if card.type == anki.cards.CARD_TYPE_REV and (card.lapses + from_lapse) >= max_fails:
+        if card.type == cards.CARD_TYPE_REV and (card.lapses + from_lapse) >= max_fails:
             show_marker(True)
 
 
-def on_did_answer(context: aqt.reviewer.Reviewer, card: anki.cards.Card, ease: int):
-    if hasattr(card, 'prevtype'):
-        if ease > 1 and card.prevtype != anki.cards.CARD_TYPE_RELEARNING and card.lapses > 0:
-            card.lapses -= 1
-            card.flush()
-            if tooltip_enabled:
-                utils.tooltip(f'Card\'s lapses set to: {card.lapses}', x_offset=400)
+def on_did_answer(context: aqt.reviewer.Reviewer, card: cards.Card, ease: int):
+    if hasattr(card, prev_type_attr):
+        if user_conf[Config.REVERSE_ENABLED] and user_conf[Config.REVERSE_METHOD] == REV_DECREASE:
+            if ease > 1 and card.__getattribute__(prev_type_attr) != cards.CARD_TYPE_RELEARNING and card.lapses > 0:
+                card.lapses -= 1
+                card.flush()
+                if tooltip_enabled:
+                    utils.tooltip(f'Card\'s lapses set to: {card.lapses}', x_offset=500)
 
-        delattr(card, 'prevtype')
+        delattr(card, prev_type_attr)
 
 
 def show_marker(show=False):
