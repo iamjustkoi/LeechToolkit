c0c7f973bed475ea1d5b98f798302c854fbc48c9
iamjustkoi <iamjustkoi@gmail.com>
2022-07-21 10:52:34 -0400

    Implemented changes for leech marker plus reformatting

--- src/reviewer.py
+++ src/reviewer.py
@@ -28,12 +28,13 @@ almost_leech_html = '''
 <div id="{marker_id}">{marker_text}</div>
 '''
 
-from_lapse = 1
 marker_id = 'leech_marker'
-marker_text = '🩸'
-marker_color = 'rgb(248, 197, 86)'
 prev_type_attr = 'prevtype'
 
+marker_text = '🩸'
+almost_color = 'rgb(248, 197, 86)'
+leech_color = 'rgb(248, 105, 86)'
+almost_leech_distance = 1
 decrease_enabled = True
 tooltip_enabled = True
 
@@ -47,9 +48,9 @@ def build_hooks():
         lambda content, context:
         on_will_start(content, context) if isinstance(context, reviewer.Reviewer) else None
     )
-    reviewer_did_show_question.append(on_did_show_question)
-    reviewer_did_show_answer.append(on_did_show_answer)
-    reviewer_did_answer_card.append(on_did_answer)
+    reviewer_did_show_question.append(on_show_front)
+    reviewer_did_show_answer.append(on_show_back)
+    reviewer_did_answer_card.append(on_answer)
 
 
 def on_will_start(content: aqt.webview.WebContent, context: aqt.reviewer.Reviewer):
@@ -58,40 +59,33 @@ def on_will_start(content: aqt.webview.WebContent, context: aqt.reviewer.Reviewe
     max_fails = conf['lapse']['leechFails']
     user_conf = LeechToolkitConfigManager(mw).config
 
-    append_html(content)
+    append_marker_html(content)
 
 
-def append_html(content: aqt.webview.WebContent):
-    if user_conf[Config.SHOW_ALMOST_LEECH_MARKER]:
-        marker_float = MARKER_POS_STYLES[user_conf[Config.ALMOST_MARK_POSITION]]
-        content.body += almost_leech_html.format(
-            marker_id=marker_id, marker_text=marker_text, marker_color=marker_color, marker_float=marker_float
-        )
+def append_marker_html(content: aqt.webview.WebContent):
+    marker_float = MARKER_POS_STYLES[user_conf[Config.MARKER_POSITION]]
+    content.body += almost_leech_html.format(
+        marker_id=marker_id, marker_text=marker_text, marker_color=leech_color, marker_float=marker_float
+    )
 
 
-def on_did_show_answer(card: cards.Card):
-    # user_conf[Config.]
-    if user_conf[Config.REVERSE_ENABLED]:
-        setattr(card, prev_type_attr, card.type)
-
-    if card.type == cards.CARD_TYPE_REV and (card.lapses + from_lapse) >= max_fails:
-        show_marker(True)
+def on_show_back(card: cards.Card):
+    setattr(card, prev_type_attr, card.type)
+    update_marker(card, False)
 
 
-def on_did_show_question(card: cards.Card):
-    print(f'CardType({CARD_TYPE_STR[card.type]})')
-    if user_conf[Config.ALMOST_ON_BACK]:
-        show_marker(False)
-    else:
-        if card.type == cards.CARD_TYPE_REV and (card.lapses + from_lapse) >= max_fails:
-            show_marker(True)
+def on_show_front(card: cards.Card):
+    # print(f'    CardType({CARD_TYPE_STR[card.type]})')
+    update_marker(card, True)
 
 
-def on_did_answer(context: aqt.reviewer.Reviewer, card: cards.Card, ease: int):
+def on_answer(context: aqt.reviewer.Reviewer, card: cards.Card, ease: int):
     if hasattr(card, prev_type_attr):
         tooltip = ''
+
         if user_conf[Config.REVERSE_ENABLED]:
             prev_type = card.__getattribute__(prev_type_attr)
+
             if user_conf[Config.REVERSE_METHOD] == REV_DECREASE:
                 if ease > 1 and card.lapses > 0 and prev_type == cards.CARD_TYPE_REV:
                     card.lapses -= 1
@@ -102,15 +96,42 @@ def on_did_answer(context: aqt.reviewer.Reviewer, card: cards.Card, ease: int):
                 if ease > 1 and card.note().has_tag(LEECH_TAG) and prev_type == cards.CARD_TYPE_REV:
                     card.note().remove_tag(LEECH_TAG)
                     card.note().flush()
-                    tooltip += f'\nCard Un-Leeched' if len(tooltip) > 0 else f'Card Un-leeched'
+                    tooltip += f'\nCard Un-Leeched' if tooltip else f'Card Un-leeched'
 
-        if tooltip_enabled and len(tooltip) > 0:
+        if tooltip_enabled and tooltip:
             utils.tooltip(tooltip, y_offset=200, x_offset=600)
+
         delattr(card, prev_type_attr)
 
 
+def set_marker_color(color: str):
+    mw.web.eval(f'document.getElementById("{marker_id}").style.textShadow = "0 0 0 {color}";')
+
+
 def show_marker(show=False):
+    """
+Changes the display state of the leech marker.
+    :param show: new visibility
+    """
     if show:
         mw.web.eval(f'document.getElementById("{marker_id}").style.display = "unset"')
     else:
         mw.web.eval(f'document.getElementById("{marker_id}").style.display = "none"')
+
+
+def update_marker(card: cards.Card, is_front: bool):
+    """
+Updates marker style/visibility based on user options and current card's attributes.
+    :param card: referenced card
+    :param is_front: current display state for card in reviewer
+    """
+    if not user_conf[Config.SHOW_LEECH_MARKER] or (user_conf[Config.ONLY_SHOW_BACK_MARKER] and is_front):
+        show_marker(False)
+    elif card.note().has_tag(LEECH_TAG):
+        set_marker_color(leech_color)
+        show_marker(True)
+    elif user_conf[Config.USE_ALMOST_MARKER] \
+            and card.type == cards.CARD_TYPE_REV \
+            and (card.lapses + almost_leech_distance) >= max_fails:
+        set_marker_color(almost_color)
+        show_marker(True)
