e0cfd193bbe3ce24c8003c1019c412a861dc45d5
iamjustkoi <jtritnum@gmail.com>
2022-07-28 08:55:20 -0400

    Added suspend action

--- src/actions.py
+++ src/actions.py
@@ -7,14 +7,6 @@ from typing import Any
 import anki.cards
 import aqt.reviewer
 
-# actions object:
-
-# init (Reviewer, Deck):
-#     deck_conf = _
-#       deck-specific
-#     user_conf = _
-#       general
-
 # do_leech (Card):
 
 # do_reverse (Card):
@@ -26,15 +18,20 @@ class LeechActionManager:
     def __init__(self, reviewer: aqt.reviewer.Reviewer, deck_id: int, user_conf: dict[str, Any]):
         # Deck Conf = (Config for Deck's Options Group or None)
         self.reviewer = reviewer
+        # self.deck_config = _
         self.user_config = user_conf
 
     # Leech actions json: action: {enabled: bool, key: val}
     def run_leech_actions(self, card: anki.cards.Card):
         for action in self.user_config[Config.LEECH_ACTIONS]:
+            print(f'ACTION {action}: {self.user_config[Config.LEECH_ACTIONS][action]}')
             if action == Action.FLAG:
-                print(f'LEECH FLAG: {self.user_config[Config.LEECH_ACTIONS][Action.FLAG]}')
                 flag_options = self.user_config[Config.LEECH_ACTIONS][Action.FLAG]
                 if flag_options[Action.ENABLED]:
                     card.set_user_flag(flag_options[Action.FLAG_INDEX])
+            if action == Action.SUSPEND:
+                sus_options = self.user_config[Config.LEECH_ACTIONS][Action.SUSPEND]
+                if sus_options[Action.ENABLED]:
+                    card.queue = -1
 
         card.flush()
