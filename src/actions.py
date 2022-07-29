0d72caaf8bb1e712b228bf932a46847498c1049b
iamjustkoi <jtritnum@gmail.com>
2022-07-29 14:11:19 -0400

    Added regex text macro to tag removal action

--- src/actions.py
+++ src/actions.py
@@ -11,6 +11,7 @@ import aqt.reviewer
 
 from .consts import Config, Action, Macro
 
+
 # do_leech (Card):
 # do_reverse (Card):
 
@@ -35,6 +36,7 @@ def get_formatted_tag(card: anki.cards.Card, tag: str):
 
 
 class LeechActionManager:
+    debug = False
 
     def __init__(self, reviewer: aqt.reviewer.Reviewer, deck_id: int, user_conf: dict[str, Any]):
         # Deck Conf = (Config for Deck's Options Group or None)
@@ -46,23 +48,38 @@ class LeechActionManager:
     def run_leech_actions(self, card: anki.cards.Card):
         leech_actions = self.user_config[Config.LEECH_ACTIONS]
         for action in leech_actions:
-            print(f'ACTION {action}: {leech_actions[action]}')
+
+            if self.debug:
+                print(f'ACTION {action}: {leech_actions[action]}')
+
             if action == Action.FLAG:
                 if leech_actions[Action.FLAG][Action.ENABLED]:
                     card.set_user_flag(leech_actions[Action.FLAG][Action.INPUT])
+
             if action == Action.SUSPEND:
                 if leech_actions[Action.SUSPEND][Action.ENABLED]:
                     card.queue = -1
+
             if action == Action.ADD_TAGS:
                 if leech_actions[Action.ADD_TAGS][Action.ENABLED]:
                     for tag in str(leech_actions[Action.ADD_TAGS][Action.INPUT]).split(' '):
                         formatted_tag = get_formatted_tag(card, tag)
                         card.note().add_tag(formatted_tag)
+
             if action == Action.REMOVE_TAGS:
                 if leech_actions[Action.REMOVE_TAGS][Action.ENABLED]:
-                    for tag in str(leech_actions[Action.REMOVE_TAGS][Action.INPUT]).split(' '):
+                    for tag in leech_actions[Action.REMOVE_TAGS][Action.INPUT].split(' '):
+                        # Formats the tag's macros then retrieves the regex pattern and replaces it from the tags string
                         formatted_tag = get_formatted_tag(card, tag)
-                        card.note().remove_tag(formatted_tag)
+                        if re.search(f'(?<!%){Macro.REGEX}:.*', formatted_tag):
+                            reg_cmd = formatted_tag.replace(f'{Macro.REGEX}:', '', 1)
+                            reg_match = re.search(f'((?<=^\")(.*)(?=\"$))|(^(?!\")(.*))', reg_cmd)
+                            reg_string = reg_match.group(0).replace(f'.', r'\S')
+                            result_tags = re.sub(fr'(?<!\S){reg_string}(?!\S)', '', card.note().string_tags()).strip()
+                            card.note().set_tags_from_str(result_tags)
+                        else:
+                            card.note().remove_tag(formatted_tag)
 
-        card.flush()
-        card.note().flush()
+        if not self.debug:
+            card.flush()
+            card.note().flush()
