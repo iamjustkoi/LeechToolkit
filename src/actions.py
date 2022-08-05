2c0830b17a966e944d0946528a967913ed725d1b
iamjustkoi <iamjustkoi@gmail.com>
2022-08-05 14:41:58 -0400

    Major reformatting

--- src/actions.py
+++ src/actions.py
@@ -45,67 +45,58 @@ class LeechActionManager:
         self.user_config = user_conf
 
     # Leech actions json: action: {enabled: bool, key: val}
-    def run_leech_actions(self, card: anki.cards.Card, debug=False):
+    def leech(self, card: anki.cards.Card, debug=False):
+        updated_card = card.col.get_card(card.id)
         leech_actions = self.user_config[Config.LEECH_ACTIONS]
 
-        for action in leech_actions:
+        if leech_actions[Action.FLAG][Action.ENABLED]:
+            updated_card.set_user_flag(leech_actions[Action.FLAG][Action.INPUT])
 
-            # print(f':    ACTION {action}: {leech_actions[action]}')
+        if leech_actions[Action.SUSPEND][Action.ENABLED]:
+            updated_card.queue = QUEUE_TYPE_SUSPENDED if leech_actions[Action.SUSPEND][Action.INPUT] else QUEUE_TYPE_REV
 
-            if action == Action.FLAG:
-                if leech_actions[Action.FLAG][Action.ENABLED]:
-                    card.set_user_flag(leech_actions[Action.FLAG][Action.INPUT])
+        if leech_actions[Action.ADD_TAGS][Action.ENABLED]:
+            for tag in str(leech_actions[Action.ADD_TAGS][Action.INPUT]).split(' '):
+                updated_card.note().add_tag(get_formatted_tag(updated_card, tag))
 
-            if action == Action.SUSPEND:
-                if leech_actions[Action.SUSPEND][Action.ENABLED]:
-                    card.queue = QUEUE_TYPE_SUSPENDED if leech_actions[Action.SUSPEND][Action.INPUT] else QUEUE_TYPE_REV
-
-            if action == Action.ADD_TAGS:
-                if leech_actions[Action.ADD_TAGS][Action.ENABLED]:
-                    for tag in str(leech_actions[Action.ADD_TAGS][Action.INPUT]).split(' '):
-                        formatted_tag = get_formatted_tag(card, tag)
-                        card.note().add_tag(formatted_tag)
-
-            if action == Action.REMOVE_TAGS:
-                if leech_actions[Action.REMOVE_TAGS][Action.ENABLED]:
-                    for tag in leech_actions[Action.REMOVE_TAGS][Action.INPUT].split(' '):
-                        # Formats the tag's macros then retrieves the regex pattern and replaces it from the tags string
-                        formatted_tag = get_formatted_tag(card, tag)
-                        if re.search(f'(?<!%){Macro.REGEX}:.*', formatted_tag):
-                            reg_cmd = formatted_tag.replace(f'{Macro.REGEX}:', '', 1)
-                            reg_match = re.search(f'((?<=^\")(.*)(?=\"$))|(^(?!\")(.*))', reg_cmd)
-                            reg_string = reg_match.group(0).replace(f'.', r'\S')
-                            result_tags = re.sub(fr'(?<!\S){reg_string}(?!\S)', '', card.note().string_tags()).strip()
-                            card.note().set_tags_from_str(result_tags)
-                        else:
-                            card.note().remove_tag(formatted_tag)
-
-            # if action == Action.EDIT_FIELDS:
-            #     if leech_actions[Action.EDIT_FIELDS][Action.ENABLED]:
-            #         for filtered_nid in leech_actions[Action.EDIT_FIELDS][Action.INPUT]:
-            #             print(f'filtered_nid: {filtered_nid}')
-            #             nid = str(filtered_nid).split('.')[0]
-            #             print(f'{card.note().id} == {nid}')
-            #             from anki.models import NotetypeId
-            #             print(f' all ids: {aqt.mw.col.models.nids(NotetypeId())}')
-            #             # TODO nid incorrect/mismatched with saved nid
-            #             if card.note().id == nid:
-            #
-            #                 meta = leech_actions[Action.EDIT_FIELDS][Action.INPUT][filtered_nid]
-            #                 print(f'meta: {meta}')
-            #
-            #                 if meta[Action.Fields.METHOD] == Action.Fields.APPEND:
-            #                     print(f'pre-card: {card.note().fields[Action.Fields.FIELD]}')
-            #                     card.note().fields[Action.Fields.FIELD] += meta[Action.Fields.FIELD]
-            #                     print(f'post-card: {card.note().fields[Action.Fields.FIELD]}')
-
-        if not debug:
-            card.flush()
-            card.note().flush()
+        if leech_actions[Action.REMOVE_TAGS][Action.ENABLED]:
+            for tag in leech_actions[Action.REMOVE_TAGS][Action.INPUT].split(' '):
+                # Formats the tag's macros then retrieves the regex pattern and replaces it from the tags string
+                formatted_tag = get_formatted_tag(updated_card, tag)
+                if re.search(f'(?<!%){Macro.REGEX}:.*', formatted_tag):
+                    reg_cmd = formatted_tag.replace(f'{Macro.REGEX}:', '', 1)
+                    reg_match = re.search(f'((?<=^\")(.*)(?=\"$))|(^(?!\")(.*))', reg_cmd)
+                    reg_string = reg_match.group(0).replace(f'.', r'\S')
+                    result_tags = re.sub(fr'(?<!\S){reg_string}(?!\S)', '', updated_card.note().string_tags()).strip()
+                    updated_card.note().set_tags_from_str(result_tags)
+                else:
+                    updated_card.note().remove_tag(formatted_tag)
 
         forget_input = leech_actions[Action.FORGET][Action.INPUT]
         if leech_actions[Action.FORGET][Action.ENABLED] and forget_input[0]:
-            forget_card(card, reset_pos=forget_input[1], reset_reviews=forget_input[2])
+            updated_card.odid = 0 if updated_card.odid else updated_card.odid
+            updated_card.odue = 0 if forget_input[1] else updated_card.odue
+            updated_card.reps, updated_card.lapses = (0, 0) if forget_input[2] else (updated_card.reps, updated_card.lapses)
+
+        # if leech_actions[Action.EDIT_FIELDS][Action.ENABLED]:
+        #     inputs: list[str] = leech_actions[Action.EDIT_FIELDS][Action.INPUT]
+        #     for filtered_nid in inputs:
+        #         nid = int(filtered_nid.split('.')[0])
+        #         if card.note_type()['id'] == nid:
+        #             print(f'note_meta: {leech_actions[Action.EDIT_FIELDS][Action.INPUT][filtered_nid]}')
+        #             note_meta = leech_actions[Action.EDIT_FIELDS][Action.INPUT][filtered_nid]
+        #             card_fields = card.note().fields
+        #
+        #             if note_meta[Action.Fields.METHOD] == Action.Fields.APPEND:
+        #                 card_fields[note_meta[Action.Fields.FIELD]] += (note_meta[Action.Fields.TEXT])
+        #             if note_meta[Action.Fields.METHOD] == Action.Fields.PREPEND:
+        #                 pass
+        #             if note_meta[Action.Fields.METHOD] == Action.Fields.REPLACE:
+        #                 pass
+        #             if note_meta[Action.Fields.METHOD] == Action.Fields.REGEX:
+        #                 pass
+
+        return updated_card
 
 
 def forget_card(card: anki.cards.Card, reset_pos=False, reset_reviews=False):
@@ -115,13 +106,3 @@ Forgets the card via a database call. Mimics Anki's methods without using its ge
     :param reset_pos: if the card's position should try to be reset to its original import position
     :param reset_reviews: if the card's lapse/review count should try to be reset back to 0
     """
-    attributes = [
-        'odid=0',
-        f'queue={QUEUE_TYPE_NEW if card.queue != QUEUE_TYPE_SUSPENDED else card.queue}'
-    ]
-    if reset_pos:
-        attributes.append('odue=0')
-    if reset_reviews:
-        attributes.append('reps=0, lapses=0')
-    cmd = f'''UPDATE cards SET {", ".join(attributes)} WHERE id in ({card.id})'''
-    card.col.db.all(cmd)
