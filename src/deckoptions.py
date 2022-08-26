ac8a627bae47fdd3849c2e97bba6b8ec204a3554
iamjustkoi <jtritnum@gmail.com>
2022-08-26 13:44:44 -0400

    Prominent fixes for some subtle issues

--- src/deckoptions.py
+++ src/deckoptions.py
@@ -17,44 +17,70 @@ from ..res.ui.deck_options import Ui_DeckOptions
 
 
 class DeckOptions(QWidget):
-    def __init__(self, deck_config):
+    def __init__(self, did: int):
         super().__init__(flags=mw.windowFlags())
+        self.did = did
         self.ui = Ui_DeckOptions()
         self.ui.setupUi(DeckOptions=self)
+        self.manager = LeechToolkitConfigManager(mw)
+
+        config = self.manager.config_for_did(did)
 
         self.reverse_widget = ReverseWidget(mw.windowFlags())
-        self.ui.leech_actions_widget = ActionsWidget(deck_config, Config.LEECH_ACTIONS, expanded=False)
-        self.ui.reverse_actions_widget = ActionsWidget(deck_config, Config.REVERSE_ACTIONS, expanded=False)
+        self.leech_actions_widget = ActionsWidget(Config.LEECH_ACTIONS, expanded=False)
+        self.reverse_actions_widget = ActionsWidget(Config.REVERSE_ACTIONS, expanded=False)
 
         self.ui.scrollAreaLayout.addWidget(self.reverse_widget)
-        self.ui.scrollAreaLayout.addWidget(self.ui.leech_actions_widget)
-        self.ui.scrollAreaLayout.addWidget(self.ui.reverse_actions_widget)
+        self.ui.scrollAreaLayout.addWidget(self.leech_actions_widget)
+        self.ui.scrollAreaLayout.addWidget(self.reverse_actions_widget)
 
-        self.ui.leech_actions_widget.load()
-        self.ui.reverse_actions_widget.load()
+    def load(self):
+        config = self.manager.config_for_did(self.did)
+        self.leech_actions_widget.load(config)
+        self.reverse_actions_widget.load(config)
+        self.reverse_widget.load(config)
+
+    def save(self):
+        config = self.manager.config_for_did(self.did)
+        config.clear()
+        self.leech_actions_widget.save(config)
+        self.reverse_actions_widget.save(config)
+        self.reverse_widget.save(config)
+
+        config_id = mw.col.decks.config_dict_for_deck_id(self.did)['id']
+        self.manager.config[config_id] = config
+        self.manager.write_config()
 
 
 def build_hooks():
     from aqt.gui_hooks import deck_conf_did_setup_ui_form
+    from aqt.gui_hooks import deck_conf_did_load_config
+    from aqt.gui_hooks import deck_conf_will_save_config
 
     deck_conf_did_setup_ui_form.append(setup_deck_options)
-    # deck_conf_did_load_config.append()
-    # deck_conf_will_save_config.append()
+    deck_conf_did_load_config.append(load_deck_options)
+    deck_conf_will_save_config.append(save_deck_options)
 
 
 def setup_deck_options(deckconf: DeckConf):
     form = deckconf.form
-    config_manager = LeechToolkitConfigManager(mw)
-    deck_config = config_manager.config_for_did(deckconf.deck['id'])
-
-    tab_options = DeckOptions(deck_config)
+    form.tab_options = DeckOptions(deckconf.deck['id'])
 
     tab_widget = form.tabWidget
     for i in range(tab_widget.count()):
         if tab_widget.tabText(i) == tr.scheduling_lapses():
-            print(f' wid: {tab_widget.tabText(i)}')
-            tab_widget.insertTab(i + 1, tab_options, 'Leech Toolkit')
+            tab_widget.insertTab(i + 1, form.tab_options, 'Leech Toolkit')
 
-    # form.tabWidget.insertTab()
+    form.tab_options.load()
+
+
+def load_deck_options(deckconf: DeckConf, *args):
+    form = deckconf.form
+    form.tab_options.load(True)
+
+
+def save_deck_options(deckconf: DeckConf, *args):
+    form = deckconf.form
+    form.tab_options.save()
 
 # On Save: Only write to global config if [key]-enabled is true else remove
