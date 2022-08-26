fd9bd33510eb5a1621d105b9d382c71bb221a1d3
iamjustkoi <jtritnum@gmail.com>
2022-08-26 07:18:22 -0400

    Added imports for deckoptions

--- __init__.py
+++ __init__.py
@@ -2,13 +2,14 @@
 MIT License: Copyright (c) 2022 JustKoi (iamjustkoi) <https://github.com/iamjustkoi>
 Full license text available in "LICENSE" file packaged with the program.
 """
-from .src import options, reviewer, bottombar
+from .src import options, reviewer, bottombar, deckoptions
 
 
 def init():
     options.bind_actions()
     reviewer.build_hooks()
     bottombar.build_bottom_bar()
+    deckoptions.build_hooks()
 
 
 init()
