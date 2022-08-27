c1933f1994af2819a4e71d26a710277423f8bded
iamjustkoi <jtritnum@gmail.com>
2022-08-27 19:09:28 -0400

    Reformatting

--- res/ui/forms.py
+++ res/ui/forms.py
@@ -1,4 +1,7 @@
+import re
+
 import aqt
+import aqt.qt
 from aqt import Qt
 from aqt.qt import (
     QLabel,
@@ -15,6 +18,7 @@ from aqt.qt import (
     QToolTip,
     QStyleOptionSlider,
     QWidget,
+    QCompleter,
 )
 
 from ...src.consts import QueueAction
@@ -142,3 +146,97 @@ class ExpandoWidget(QWidget):
         super(ExpandoWidget, self).mousePressEvent(event)
         if self.click_function:
             self.click_function()
+
+
+class CustomCompleter(QCompleter):
+
+    def __init__(self, parent_line_edit: aqt.qt.QLineEdit) -> None:
+        QCompleter.__init__(self, aqt.qt.QStringListModel(), parent_line_edit)
+
+        self.current_data: list[str] = []
+        self.cursor_index: int or None = None
+        self.cursor_item_pos: int or None = None
+
+        self.line_edit = parent_line_edit
+
+        self.setCaseSensitivity(Qt.CaseSensitivity.CaseInsensitive)
+        self.setFilterMode(Qt.MatchContains)
+        self.setCompletionPrefix(' ')
+
+        def focus_event(event):
+            default_focus_evt(event)
+            if len(self.line_edit.text()) <= 0:
+                self.complete()
+
+        def release_event(event):
+            default_release_evt(event)
+            if len(self.line_edit.text()) <= 0:
+                self.complete()
+
+        default_focus_evt = self.line_edit.focusInEvent
+        default_release_evt = self.line_edit.mouseReleaseEvent
+        self.line_edit.focusInEvent = focus_event
+        self.line_edit.mouseReleaseEvent = release_event
+
+    def set_list(self, data: list[str]):
+        self.setModel(aqt.qt.QStringListModel(data))
+
+    def get_path_pos(self):
+        return sum([len(item) for item in self.current_data[:self.cursor_index]])
+
+    def splitPath(self, path: str) -> list[str]:
+        """
+    Splits the line edit's path based on a variety of filters, updates the current cursor position variables,
+    and outputs a list with a single item to use as
+    auto-completion suggestions.
+        :param path: the current path to split/filter
+        :return: a list containing a single string to use as a reference for completer suggestions
+        """
+        formatted_path = re.sub('  +', ' ', path)
+        stripped_path = formatted_path.strip()
+        cursor_pos = self.line_edit.cursorPosition()
+        self.cursor_index = stripped_path.count(' ', 0, cursor_pos)
+        self.current_data = formatted_path.strip().split(' ')
+        self.cursor_item_pos = cursor_pos - self.get_path_pos()
+
+        if formatted_path.endswith(' ') and cursor_pos >= len(formatted_path):
+            self.current_data.append('')
+            self.cursor_index += 1
+            return ['']
+
+        if cursor_pos == 0:
+            self.current_data.insert(0, '')
+            self.cursor_index = 0
+            return ['']
+
+        item_macro_pos = self.current_data[self.cursor_index].rfind('%', 1)
+        if self.cursor_item_pos > item_macro_pos > 0:
+            return [self.current_data[self.cursor_index][item_macro_pos:]]
+
+        return [self.current_data[self.cursor_index]]
+
+    def pathFromIndex(self, index: aqt.qt.QModelIndex) -> str:
+        """
+    Retrieves the line edit's path from the given index data.
+        :param index: QModelIndex used as a reference for what to insert
+        :return: the string output of the given path result
+        """
+        if self.cursor_index is None:
+            return self.line_edit.text()
+
+        current_item = self.current_data[self.cursor_index]
+        item_macro_pos = self.current_data[self.cursor_index].rfind('%', 1)
+
+        if self.cursor_item_pos > item_macro_pos > 0:
+            self.current_data[self.cursor_index] = current_item[:item_macro_pos] + index.data()
+        else:
+            self.current_data[self.cursor_index] = index.data()
+
+        def update_cursor_pos():
+            raw_pos = self.get_path_pos() + len(self.current_data[:self.cursor_index])
+            self.line_edit.setCursorPosition(len(self.current_data[self.cursor_index]) + raw_pos)
+
+        # Timer to update cursor after completion (delayed)
+        aqt.qt.QTimer(aqt.mw).singleShot(0, update_cursor_pos)
+
+        return ' '.join(self.current_data)
