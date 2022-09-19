1693f5e6fc92c7d53d4fceb498c06f8902fff38a
iamjustkoi <iamjustkoi@gmail.com>
2022-09-19 11:38:15 -0400

    Added sync module

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@
 MIT License: Copyright (c) 2022 JustKoi (iamjustkoi) <https://github.com/iamjustkoi>
 Full license text available in "LICENSE" file packaged with the program.
 """
-from .src import options, reviewer, bottombar, deckoptions
+from .src import options, reviewer, bottombar, deckoptions, sync
 
 
 def init():
@@ -10,6 +10,7 @@ def init():
     reviewer.build_hooks()
     bottombar.build_bottom_bar()
     deckoptions.build_hooks()
+    sync.build_hooks()
 
 
 init()
