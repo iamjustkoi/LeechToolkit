0e067d6c4a4ee2a2496f456a6ca32ac1fa012635
iamjustkoi <jtritnum@gmail.com>
2022-09-23 22:31:36 -0400

    Removed init method to reduce complexity

--- __init__.py
+++ __init__.py
@@ -4,13 +4,8 @@ Full license text available in "LICENSE" file packaged with the program.
 """
 from .src import options, reviewer, bottombar, deckoptions, sync
 
-
-def init():
-    options.bind_actions()
-    reviewer.build_hooks()
-    bottombar.build_bottom_bar()
-    deckoptions.build_hooks()
-    sync.build_hooks()
-
-
-init()
+options.bind_actions()
+reviewer.build_hooks()
+bottombar.build_bottom_bar()
+deckoptions.build_hooks()
+sync.build_hooks()
