f31185c851a17bfd7680fa818fe5772c4965841b
iamjustkoi <iamjustkoi@gmail.com>
2022-09-24 00:20:48 -0400

    Added browser inits

--- __init__.py
+++ __init__.py
@@ -2,10 +2,11 @@
 MIT License: Copyright (c) 2022 JustKoi (iamjustkoi) <https://github.com/iamjustkoi>
 Full license text available in "LICENSE" file packaged with the program.
 """
-from .src import options, reviewer, bottombar, deckoptions, sync
+from .src import options, reviewer, bottombar, deckoptions, sync, browser
 
 options.bind_actions()
 reviewer.build_hooks()
 bottombar.build_bottom_bar()
 deckoptions.build_hooks()
 sync.build_hooks()
+browser.build_hooks()
