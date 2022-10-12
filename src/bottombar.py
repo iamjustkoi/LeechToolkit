b206a3d23117579f0070450847756f7ddc573a1f
iamjustkoi <jtritnum@gmail.com>
2022-10-12 17:31:01 -0400

    Fixed various issues with bad syncing checks and flushing. Also added tooltip on sync completion.

--- src/bottombar.py
+++ src/bottombar.py
@@ -33,7 +33,7 @@ def build_bottom_bar():
 
                 def leech_link_handler(url):
                     """
-                    Custom link handler that adds functionality for the run_action-browse button's link.
+                    Custom link handler that adds functionality for the handle_input_action-browse button's link.
                     :param url: passed url string to handle
                     """
                     if url == LEECHES_URL:
