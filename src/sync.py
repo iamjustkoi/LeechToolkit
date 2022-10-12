5aad34e81ed5e86124895c37d09ada51afd672be
iamjustkoi <iamjustkoi@gmail.com>
2022-10-12 17:31:01 -0400

    Fixed various issues with bad syncing checks and flushing. Also added tooltip on sync completion.

--- src/sync.py
+++ src/sync.py
@@ -6,7 +6,7 @@ import traceback
 
 from aqt import gui_hooks, mw
 
-from .updates import run_action_updates, update_card, is_unique_card
+from .actions import handle_actions
 from .config import LeechToolkitConfigManager, merge_fields
 from .consts import ANKI_SYNC_ISSUE_VER, ANKI_UNDO_UPDATE_VER, CURRENT_ANKI_VER, Config, ErrorMsg, LEECH_TAG, String
 from anki.consts import *
@@ -20,6 +20,16 @@ except ModuleNotFoundError:
 def build_hooks():
     if CURRENT_ANKI_VER != ANKI_SYNC_ISSUE_VER:
         gui_hooks.sync_did_finish.append(sync_collection)
+
+        from aqt.undo import UndoActionsInfo
+
+        def print_undo_state(info: UndoActionsInfo):
+            print(f'    .--undo infO--.')
+            print(f'    {info}')
+            print(f'    last_step: {mw.col.undo_status().last_step}')
+            print()
+
+        gui_hooks.undo_state_did_change.append(print_undo_state)
 
 
 def get_remeasured_lapses(cid: int, reverse_conf: dict):
@@ -36,7 +46,6 @@ def get_remeasured_lapses(cid: int, reverse_conf: dict):
     reviews = mw.col.db.all(f'SELECT ease, type, ivl FROM revlog WHERE cid={cid} ORDER BY id DESC')
 
     for ease, rev_type, ivl in reviews:
-
         # If review type was relearn or learn, reset the consecutive count
         if rev_type not in (REVLOG_RELRN, REVLOG_LRN):
             consecutive_correct = 0
@@ -46,7 +55,6 @@ def get_remeasured_lapses(cid: int, reverse_conf: dict):
             remeasured_lapses = 0
 
         if rev_type == REVLOG_REV:
-
             if ease < BUTTON_TWO:
                 remeasured_lapses += 1
                 consecutive_correct = 0
@@ -61,15 +69,14 @@ def get_remeasured_lapses(cid: int, reverse_conf: dict):
     return remeasured_lapses
 
 
-def sync_collection():
+def sync_collection(is_manual_sync=False):
     """
     Syncs the collection's lapse count and, optionally, leech status based on the current user preferences and review
     logs.
     """
     global_conf = LeechToolkitConfigManager(mw).config
 
-    if global_conf[Config.SYNC_ENABLED]:
-
+    if global_conf[Config.SYNC_ENABLED] or is_manual_sync:
         # Stash config data
         toolkit_configs: dict = {}
         thresholds: dict = {}
@@ -82,61 +89,51 @@ def sync_collection():
         cards = mw.col.db.all(f'SELECT id, did, lapses, type, nid FROM cards WHERE reps > 0 ORDER BY id DESC')
 
         # Stored for double-checking siblings not updated, yet
-        tagged_nids = []
+        tagged_nids = set()
+        updated_cids = set()
 
         for cid, did, lapses, card_type, nid in cards:
-
-            # Only updating status for cards in the review queue; otherwise letting User/Anki handle updates.
+            # Only updates status for cards in the review queue; otherwise handled by User/Anki.
             if card_type == QUEUE_TYPE_REV:
-                updated_card = mw.col.get_card(cid)
-                updated_note = updated_card.note()
-
+                card = mw.col.get_card(cid)
                 toolkit_config = toolkit_configs[str(did)]
 
                 if toolkit_config[Config.REVERSE_OPTIONS][Config.REVERSE_ENABLED]:
-                    updated_card.lapses = max(get_remeasured_lapses(cid, toolkit_config[Config.REVERSE_OPTIONS]), 0)
+                    card.lapses = max(get_remeasured_lapses(cid, toolkit_config[Config.REVERSE_OPTIONS]), 0)
+
+                if card.lapses != lapses:
+                    updated_cids.add(cid)
 
                 threshold = thresholds[str(did)] \
                     if toolkit_config[Config.REVERSE_OPTIONS][Config.REVERSE_USE_LEECH_THRESHOLD] \
                     else toolkit_config[Config.REVERSE_OPTIONS][Config.REVERSE_THRESHOLD]
 
-                has_toolkit_tag = updated_note.has_tag(String.SYNC_TAG_DEFAULT) and nid not in tagged_nids
-                has_leech_tag = updated_note.has_tag(LEECH_TAG)
+                has_toolkit_tag = card.note().has_tag(String.SYNC_TAG_DEFAULT) and nid not in tagged_nids
+                has_leech_tag = card.note().has_tag(LEECH_TAG)
 
-                if toolkit_config[Config.REVERSE_OPTIONS][Config.REVERSE_ENABLED] and updated_card.lapses < threshold:
-
+                if toolkit_config[Config.REVERSE_OPTIONS][Config.REVERSE_ENABLED] and card.lapses < threshold:
                     # Unleech
                     if toolkit_config[Config.SYNC_TAG_OPTIONS][Config.SYNC_TAG_ENABLED] and has_toolkit_tag:
-                        run_action_updates(
-                            updated_card,
-                            toolkit_config,
-                            Config.UN_LEECH_ACTIONS,
-                            reload=False
-                        )
+                        card = handle_actions(card, toolkit_config, Config.UN_LEECH_ACTIONS, reload=False)
+                        updated_cids.add(cid)
 
                     if has_leech_tag:
-                        updated_note.remove_tag(LEECH_TAG)
+                        card.note().remove_tag(LEECH_TAG)
+                        updated_cids.add(cid)
 
-                elif updated_card.lapses >= threshold:
-
+                elif card.lapses >= threshold:
                     # Leech
                     if toolkit_config[Config.SYNC_TAG_OPTIONS][Config.SYNC_TAG_ENABLED] and not has_toolkit_tag:
-                        run_action_updates(
-                            updated_card,
-                            toolkit_config,
-                            Config.LEECH_ACTIONS,
-                            reload=False
-                        )
-
-                        if nid not in tagged_nids:
-                            tagged_nids.append(nid)
+                        handle_actions(card, toolkit_config, Config.LEECH_ACTIONS, reload=False)
+                        tagged_nids.add(nid) if nid not in tagged_nids else None
+                        updated_cids.add(cid)
 
                     if not has_leech_tag:
-                        updated_note.add_tag(LEECH_TAG)
+                        card.note().add_tag(LEECH_TAG)
+                        updated_cids.add(cid)
 
-                if is_unique_card(mw.col.get_card(cid), updated_card):
-                    if CURRENT_ANKI_VER >= ANKI_UNDO_UPDATE_VER:
-                        update_card(updated_card, aqt.operations.OpChanges)
-                    else:
-                        update_card(updated_card)
-                        mw.checkpoint(String.ENTRY_LEECH_ACTIONS)
+                if cid in updated_cids:
+                    card.flush()
+                    card.note().flush()
+
+        aqt.utils.tooltip(String.TIP_UPDATED_TEMPLATE.format(len(updated_cids)))
